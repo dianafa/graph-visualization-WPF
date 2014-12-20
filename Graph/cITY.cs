32e74f725451606ae39fbf6a1a674cde4b129e16
dianafa <diana.falkowska@gmail.com>
2014-12-20 21:56:30 +0100

    init

---
+++ Graph/cITY.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class City
+    {
+        string name;
+        string county;
+        string population;
+    }
+
+}
