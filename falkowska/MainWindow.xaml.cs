201cf980e04165093a16237fcd8aa09f5fa8b2b4
dianafa <diana.falkowska@gmail.com>
2015-01-11 16:58:03 +0100

    still UI

--- falkowska/MainWindow.xaml.cs
+++ falkowska/MainWindow.xaml.cs
@@ -25,23 +25,35 @@ namespace falkowska
         public MainWindow()
         {
             InitializeComponent();
-            //this.DataContext = this;
+            this.DataContext = this;
             cityGraph = new Graph<City>();
             cityGraph["Poznan"] = new City("Poznan", "wielkopolskie", "547tys.");
+            lbNodes.ItemsSource = cityGraph;
+
         }
 
         private void MainGrid_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
-            MessageBox.Show("Ile nodow: " + cityGraph.nodes.Count);
+            //MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+            MessageBox.Show("cityGraph.nodeCount: " + cityGraph.nodes.Count);
         }
         private void AddNode(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(" cityGraph[Poznan].name: " + cityGraph["Poznan"].name);
-            //cityGraph["Poznan"] = new City("Poznan", "wielkopolskie", "547tys.");
+            cityGraph["Bydgoszcz"] = new City("Bydgoszcz", "kujawsko-pomorskie", "297tys.");
             cityGraph.AddNode(new City("Krakow", "wielkopolskie", "647tys."));
             MessageBox.Show("cityGraph.nodeCount: " + cityGraph.nodes.Count);
             //cityGraph.AddNode(Node<City> node)
+        }
+
+        private void EditNode(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Edytujesz noda: ");
+        }
+
+        private void RemoveNode(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Usuwasz noda: ");
         }
     }
 }
