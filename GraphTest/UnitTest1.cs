b0a79fa9b1d0dbc648875685220d9bf8eb0fcac7
dianafa <diana.falkowska@gmail.com>
2015-01-29 09:35:43 +0100

    UnitTest template added

---
+++ GraphTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphLibrary;
+
+namespace GraphLibraryTest
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
